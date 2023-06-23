649e15381cfc7b1635fe54460c3ed7e7088892e6
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2023-06-23 15:42:18 +0500

    fix: remove all bootstrap library

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import './assets/main.scss'
-import { createApp, Vue } from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { BootstrapVue } from 'bootstrap-vue'
 
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 const app = createApp(App)
-Vue.use(BootstrapVue)
 app.use(createPinia())
 app.use(router)
 
