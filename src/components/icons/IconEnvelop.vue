aa7555153278f4482dbfd2aaa4e0a7e0b755338c
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2023-07-04 14:09:30 +0500

    fix: icon envelop

--- src/components/icons/IconEnvelop.vue
+++ src/components/icons/IconEnvelop.vue
@@ -1,6 +1,6 @@
 <template>
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1.71387 6.28571C1.71387 5.02335 2.73722 4 3.99958 4H19.9996C21.2619 4 22.2853 5.02335 22.2853 6.28571V17.7143C22.2853 18.9767 21.2619 20 19.9996 20H3.99958C2.73722 20 1.71387 18.9767 1.71387 17.7143V6.28571Z" stroke="#14181F" stroke-width="1.37143" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22.2853 6.28564L11.9996 13.1428L1.71387 6.28564" stroke="#14181F" stroke-width="1.37143" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1.71387 6.28571C1.71387 5.02335 2.73722 4 3.99958 4H19.9996C21.2619 4 22.2853 5.02335 22.2853 6.28571V17.7143C22.2853 18.9767 21.2619 20 19.9996 20H3.99958C2.73722 20 1.71387 18.9767 1.71387 17.7143V6.28571Z" stroke="#14181F" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M22.2853 6.28564L11.9996 13.1428L1.71387 6.28564" stroke="#14181F" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </template>
